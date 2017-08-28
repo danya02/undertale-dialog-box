139e9d6e0705e6daf5f7e7c385ee8036a91338df
Danya Generalov <danya02@me.com>
2017-08-28 19:54:34 +0300

    Adding function to add text.

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def generate_main_frame():
     output.fill(pygame.Color("#000000"), pygame.Rect(14, 14, 567, 139))
     return output
 
+def insert_text(surface, line, text):
+    font = pygame.font.Font(pygame.font.match_font("determinationmonoweb"), 32)
+    if line == 1:
+        start_position = (188, 31)
+    elif line == 2:
+        start_position = (188, 67)
+    elif line == 3:
+        start_position = (188, 103)
+    surface.blit(font.render(text, 0, pygame.Color("#ffffff"))
+
 if __name__ == "__main__":
     d = pygame.display.set_mode((596, 168))
     d.blit(generate_main_frame(), (0,0))
